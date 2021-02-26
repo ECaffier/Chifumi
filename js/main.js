fd924b37e60d3b5a05492a5645b6066f6e42d8d1
Caffier Edouard <caffier.ed@gmail.com>
2021-02-26 09:00:51 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Message de bienvenue
+alert("Bienvenue sur le site du Shifumi !");
+//Demande le nom et le stock dans la variable name
+var name = prompt("Si vous souhaitez jouer, rentrez votre nom");
+//Utilise la variable name pour demander si le joueur est pret.
+alert ("Es tu prêt  " + name + "?")
+//Demande le choix du joueur et le stock dans la variable choice
+var choice= prompt("Choisis pierre, feuille ou ciseaux.")
